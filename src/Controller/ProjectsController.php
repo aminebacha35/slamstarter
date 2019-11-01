871417850fee6123aca7ba37e0c0aeeb35395435
bachami <you@example.com>
2019-11-01 23:15:31 +0100

    first commit

---
+++ src/Controller/ProjectsController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ProjectsController extends AbstractController
+{
+    /**
+     * @Route("/projects", name="projects")
+     */
+    public function index()
+    {
+        return $this->render('projects/index.html.twig', [
+            'controller_name' => 'ProjectsController',
+             'project' => ["project1","project2","project3"] 
+        ]);
+    }
+}
